The VAE is fixed. Normalization method is changed. The dataset is changed. A new VAE model is trained based on these changes.

Example_Use/src/data_loading.py
@@ -58,6 +58,7 @@ def compute_aggregate_min_max(min_val, max_val, shape=(110, 70), num_sc_per_rb=1
     return min_aggregate_val, max_aggregate_val
 
 
+
 # Data Classes ------------------------------------------------
 
 class SpectralImagesUnsupervised(Dataset):
@@ -65,15 +66,27 @@ class SpectralImagesUnsupervised(Dataset):
     Dataset class for loading spectrogram image patches for an Unsupervised Learning task.
     """
 
-    def __init__(self, path: str, transform, dataframe, load_dt_images=False):
+    def __init__(self, path: str, transform, dataframe, load_dt_images=False, data_type="train", max=None, min=None):
 
         if load_dt_images:
             raise NotImplementedError("Loading dt_images is currently not implemented.")
 
+        if data_type != "train":
+            if max is None:
+                raise ValueError(f"max value in {data_type} dataset must not be None.")
+            if min is None:
+                raise ValueError(f"min value in {data_type} dataset must not be None.")
+                
+            self.max = max
+            self.min = min
+        else:
+            self.max = None
+            self.min = None
+        
         self.transform = transform  
         self.dir = os.path.join(path, "Images")  # directory where spectrogram PNGs are stored
         self.id = []  
-        self.frames = []  
+        self.frames = []
         self.jammer = []  # list to hold jam type labels (string initially, mapped later)
         self.num_transmitters = []  # list to hold number of legitimate transmitters for each example
         self.jammer_power = []  # list to hold jammer power value for each example
@@ -81,7 +94,7 @@ class SpectralImagesUnsupervised(Dataset):
         # Load ospectrogram global min/max from metadata CSV
         min_val, max_val = load_spectrogram_min_max(path)
         # Compute min/max after the same aggregation used for inputs (so normalization is consistent)
-        min_aggregate_val, max_aggregate_val = compute_aggregate_min_max(min_val, max_val)
+        #min_aggregate_val, max_aggregate_val = compute_aggregate_min_max(min_val, max_val)
 
         if os.path.isdir(self.dir):
             image_files = set(os.listdir(self.dir)) 
@@ -104,20 +117,37 @@ class SpectralImagesUnsupervised(Dataset):
                     img = aggregate_subcarriers(img, num_sc_per_rb=12)
 
                     # Normalize aggregated image to [0, 1] using aggregated min/max
-                    img = (img - min_aggregate_val) / (max_aggregate_val - min_aggregate_val)
+                    #img = (img - min_aggregate_val) / (max_aggregate_val - min_aggregate_val)
 
                     # If a transform is provided, apply it (albumentations expects keyword 'image')
-                    if self.transform is not None:
-                        img_transformed = self.transform(image=img)
-                        self.frames.append(img_transformed["image"])
-                    else:
-                        self.frames.append(img)
+                    self.frames.append(img)
 
                     self.jammer.append(dataframe.loc[dataframe['name'] == n, 'jammer_type'].iloc[0])
                     self.num_transmitters.append(
                         dataframe.loc[dataframe['name'] == n, 'num_legitimate_transmitters'].iloc[0]
                     )
                     self.jammer_power.append(dataframe.loc[dataframe['name'] == n, 'jammer_power'].iloc[0])
+
+            if data_type == "train":
+                train_max = np.max(self.frames)
+                train_min = np.min(self.frames)
+                r = train_max-train_min
+                g = r/0.8
+
+                self.min = train_min - 0.1*g
+                self.max = train_max + 0.1*g
+
+            print(f"maximum and minimum values used for normalisation: {self.max}, {self.min} ")
+
+            for i, frame in enumerate(tqdm(self.frames, desc="normalisation")):
+                img = (frame - self.min) / (self.max - self.min)
+                np.clip(img, 0.0, 1.0, out=img)
+
+                if self.transform is not None:
+                    img = self.transform(image=img)["image"]
+
+                self.frames[i] = img
+
 
             # Print available label distribution (strings) for debugging
             print("Sample Labels: \n", np.unique(self.jammer, return_counts=True))
@@ -246,11 +276,19 @@ class SpectralImagesSample(Dataset):
     Dataset class for loading spectrogram images from a certain sample.
     """
 
-    def __init__(self, sample_id, path: str, transform, dataframe, load_dt_images=False):
+    def __init__(self, sample_id, path: str, transform, dataframe, load_dt_images=False, max=None, min=None):
 
         if load_dt_images:
             raise NotImplementedError("Loading dt_images is currently not implemented.")
-
+        
+        if max is None:
+            raise ValueError(f"max value must not be None.")
+        if min is None:
+            raise ValueError(f"min value must not be None.")
+            
+        self.max = max
+        self.min = min
+    
         self.transform = transform  
         self.dir = os.path.join(path, "Images")  # directory where spectrogram PNGs are stored
         self.id = []
@@ -261,8 +299,8 @@ class SpectralImagesSample(Dataset):
 
         # Load ospectrogram global min/max from metadata CSV
         min_val, max_val = load_spectrogram_min_max(path)
-        # Compute min/max after the same aggregation used for inputs (so normalization is consistent)
-        min_aggregate_val, max_aggregate_val = compute_aggregate_min_max(min_val, max_val)
+        ## Compute min/max after the same aggregation used for inputs (so normalization is consistent)
+        #min_aggregate_val, max_aggregate_val = compute_aggregate_min_max(min_val, max_val)
 
         if os.path.isdir(self.dir):
 
@@ -277,15 +315,7 @@ class SpectralImagesSample(Dataset):
                 img = (img / 255) * (max_val - min_val) + min_val
                 img = aggregate_subcarriers(img, num_sc_per_rb=12)
 
-                # Normalize aggregated image to [0, 1] using aggregated min/max
-                img = (img - min_aggregate_val) / (max_aggregate_val - min_aggregate_val)
-
-                # If a transform is provided, apply it (albumentations expects keyword 'image')
-                if self.transform is not None:
-                    img_transformed = self.transform(image=img)
-                    self.frames.append(img_transformed["image"])
-                else:
-                    self.frames.append(img)
+                self.frames.append(img)
 
                 self.jammer.append(dataframe.loc[dataframe['name'] == sample_id, 'jammer_type'].iloc[0])
                 self.num_transmitters.append(
@@ -295,6 +325,17 @@ class SpectralImagesSample(Dataset):
 
             # Print available label distribution (strings) for debugging
             print("Sample Labels: \n", np.unique(self.jammer, return_counts=True))
+
+            print(f"maximum and minimum values used for normalisation: {self.max}, {self.min} ")
+
+            for i, frame in enumerate(tqdm(self.frames, desc="normalisation")):
+                img = (frame - self.min) / (self.max - self.min)
+                np.clip(img, 0.0, 1.0, out=img)
+
+                if self.transform is not None:
+                    img = self.transform(image=img)["image"]
+
+                self.frames[i] = img
 
     def __len__(self) -> int:
         # Return number of cached frames

Example_Use/src/models.py
@@ -159,7 +159,7 @@ class Decoder(nn.Module):
     def __init__(
         self,
         latent_dim=latent_dim,
-        base_channels=16,
+        base_channels=32,
         img_channels=1,
         img_height=img_size_time,
         img_width=img_size_freq,

Example_Use/src/train_unsupervised.py
@@ -51,7 +51,7 @@ base_transform = trans.Compose(
 
 train_transform = trans.Compose(
     [
-        trans.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+        #trans.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
         base_transform,
     ]
 )
@@ -72,6 +72,7 @@ def get_dataset(root, df, load_dt_images=True):
         transform=train_transform,
         dataframe=train_samples,
         load_dt_images=load_dt_images,
+        data_type="train",
     )
 
     normal_valid_samples = normal[num_train_normal:num_train_normal+num_valid_normal]
@@ -80,6 +81,9 @@ def get_dataset(root, df, load_dt_images=True):
         transform=base_transform,
         dataframe=normal_valid_samples,
         load_dt_images=load_dt_images,
+        data_type="valid",
+        max = trainset.max,
+        min = trainset.min,
     )
 
     trainloader = DataLoader(
@@ -99,6 +103,7 @@ def get_dataset(root, df, load_dt_images=True):
     )
 
     return trainloader, normal_validloader
+
 
 
 # ------------------------------------------------
@@ -149,16 +154,17 @@ def train(
 
                 optimizer.zero_grad()
 
-                mu, sigma = encoder(combined_inputs)
-                epsilon = torch.randn_like(sigma).to(device)
-                z = mu + sigma * epsilon
+                mu, log_var = encoder(combined_inputs)
+                std = torch.exp(0.5 * log_var)
+                epsilon = torch.randn_like(std).to(device)
+                z = mu + std * epsilon
                 decoded_x = decoder(z)
                 r_loss = reconstruction_loss(
                     decoded_x, combined_targets
                     )
-                kl = (
-                    -0.5 * torch.sum(1 + sigma - mu.pow(2) - sigma.exp())
-                    ) / orig_img.shape[0]
+                kl = -0.5 * torch.mean(
+                    1 + log_var - mu.pow(2) - log_var.exp()
+                    )
                 
                 loss = r_loss + 0.0001 * kl
 
@@ -169,8 +175,9 @@ def train(
                 optimizer.step()
 
 
-                if scheduler != None:
-                    scheduler.step()
+                #if scheduler != None:
+                #    scheduler.step()
+            scheduler.step()
 
             log.add_scalar("Rec_Loss/Train", np.nanmean(r_list), global_step=epoch)
             log.add_scalar("KL_Loss/Train", np.nanmean(kl_list), global_step=epoch)
@@ -189,15 +196,15 @@ def train(
                     img = img.float()
                     img = img.to(device)
 
-                    mu, sigma = encoder(img)
-                    epsilon = torch.randn_like(sigma).to(device)
-                    z = mu + sigma * epsilon
+                    mu, log_var = encoder(img)
+                    std = torch.exp(0.5 * log_var)
+                    epsilon = torch.randn_like(std).to(device)
+                    z = mu + std * epsilon
                     decoded_x = decoder(z)
                     v_r_loss = reconstruction_loss(decoded_x, img)
-                    v_kl = (
-                        -0.5 * torch.sum(1 + sigma - mu.pow(2) - sigma.exp())
-                    ) / img.shape[0]
-
+                    v_kl = -0.5 * torch.mean(
+                        1 + log_var - mu.pow(2) - log_var.exp()
+                        )
                     valid_Rec_loss.append(v_r_loss.item())
                     valid_kl_loss.append(v_kl.item())
 
@@ -261,7 +268,7 @@ if __name__ == "__main__":
 
     except Exception as e:
         print(e)
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
         print("Device:", device)
         Multiple_GPU = False
 
@@ -278,14 +285,19 @@ if __name__ == "__main__":
     decoder = decoder.to(device)
     
     n_epochs = config["hyperparameters"]["unsupervised"]["n_epochs"]
-    reconstruction_loss = nn.BCELoss()
+    reconstruction_loss = nn.L1Loss()
     optimizer = torch.optim.Adam(
-            itertools.chain(encoder.parameters(), decoder.parameters()), lr=0.1
+            itertools.chain(encoder.parameters(), decoder.parameters()), lr=max_lr
         )
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(
-            optimizer, max_lr, epochs=n_epochs, steps_per_epoch=len(trainloader), pct_start=0.2,
-            )
+    #scheduler = torch.optim.lr_scheduler.OneCycleLR(
+    #        optimizer, max_lr, epochs=n_epochs, steps_per_epoch=len(trainloader), pct_start=0.2,
+    #        )
 
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer,
+            milestones=[40, 100],
+            gamma=0.1  # Multiply LR by 0.1 at each milestone
+        )
     # ------------------------------------------------
     # Training Loop
     # ------------------------------------------------